25d90dd4afc78cf1a133d227a7220dd4a0023abd
kazuki1023 <k.keio12569@gmail.com>
2024-06-18 01:04:42 +0900

    feat: :sparkles: health終了

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
@@ -16,5 +18,17 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	// `model`パッケージの構造体を使用
+	response := model.HealthzResponse{Message: "OK"}
+
+	// レスポンスのContent-Typeを設定
+	w.Header().Set("Content-Type", "application/json")
+
+	// Encoder を使用してJSONにシリアライズし、HTTPレスポンスに書き込む
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		// エラーハンドリング
+		log.Println(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
