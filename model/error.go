55493480f8a8bd185195a29a846c3556679d8104
kazuki1023 <k.keio12569@gmail.com>
2024-06-19 02:28:37 +0900

    feat: :sparkles: &todo

--- model/error.go
+++ model/error.go
@@ -1,5 +1,13 @@
 package model
 
-type ErrNotFound interface {
-	Error() string
+// ErrNotFound represents a not found error.
+type ErrNotFound struct{}
+
+func (e *ErrNotFound) Error() string {
+	return "not found"
+}
+
+// NewErrNotFound returns a new instance of ErrNotFound.
+func NewErrNotFound() error {
+	return &ErrNotFound{}
 }
