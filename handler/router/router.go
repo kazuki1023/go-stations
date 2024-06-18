fd8fa08d22ab97f8e89853c3b73fdf9992dd65d1
kazuki1023 <k.keio12569@gmail.com>
2024-06-19 00:43:44 +0900

    feat: :sparkles: healthのhandlerを定義

--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,25 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+
+	// HealthzHandler の登録
+	healthzHandler := handler.NewHealthzHandler()
+	mux.Handle("/healthz", healthzHandler)
+
+	//  TODOHandlerの作成
+	todoService := service.NewTODOService(todoDB)
+	todoHandler := handler.NewTODOHandler(todoService)
+
+	// エンドポイントの登録
+	mux.Handle("/todos", todoHandler)
+
 	return mux
 }
